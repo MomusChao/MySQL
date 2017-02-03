bcff891c123fc9aad7194525cbf22059827d7a8f
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-02-03 09:33:34 +0800

    Change_Table_add_appoint

---
+++ 01Basic/14Change_Table_add_appoint.sql
@@ -0,0 +1,6 @@
+#14 Change Table_add_appoint
+
+#ALTER TABLE tablename ADD Field01 DataType AFTER Field02;
+ALTER TABLE myfirstdb ADD Address VARCHAR(10) AFTER RealAge;
+#show table
+DESC myfirstdb;
