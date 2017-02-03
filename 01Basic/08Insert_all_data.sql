871ed963a584264c65fb6fa8d44b80837a0cec1c
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-02-03 09:31:42 +0800

    Insert_all_data

---
+++ 01Basic/08Insert_all_data.sql
@@ -0,0 +1,4 @@
+#08 Insert all data
+
+#INSERT INTO tablename (Field, Field, ...) VALUES (data1,data2,...),(data1,data2,...)
+INSERT INTO myfirstdb (ID,RealName,RealAge) VALUES ('O903','Eva',24), ('O904','Steven',21), ('O905','Ellen',25);
