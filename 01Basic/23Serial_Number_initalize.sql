4eb390fed84fbcc87953492f8dced983ab867b81
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-02-04 10:44:56 +0800

    Serial_Number_initalize

---
+++ 01Basic/23Serial_Number_initalize.sql
@@ -0,0 +1,7 @@
+#23 Serial Number_initalize
+
+#ALTER TABLE TableName AUTO_INCREMENT=0;
+ALTER TABLE myfirstdb04 AUTO_INCREMENT=0;
+
+INSERT INTO myfirstdb04  VALUES(1,'Michael');
+SELECT * FROM myfirstdb04;
