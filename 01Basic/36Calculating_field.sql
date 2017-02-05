0f9b615567bb2ffe6f15b5aac3df0f602290ff9d
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-02-05 15:20:13 +0800

    Calculating_field

---
+++ 01Basic/36Calculating_field.sql
@@ -0,0 +1,6 @@
+#36 calculating field
+
+SELECT uria, uria+50 AS Sales_Performance FROM TableMo;
+SELECT uria, uria-5 AS Sales_Performance FROM TableMo;
+SELECT uria, uria*10 AS Sales_Performance FROM TableMo;
+SELECT uria, uria/10 AS "Sales_Performance (US)" FROM TableMo;
