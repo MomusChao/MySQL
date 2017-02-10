2d8bf3df55438ccedd9f756b2b77205952518a6a
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2017-02-10 17:09:40 +0800

    Stored_function__show_&_delete

---
+++ 02Advance/106Stored_function__show_&_delete.sql
@@ -0,0 +1,9 @@
+#106 Stored function: show & delete
+
+#show Stored function
+SHOW CREATE FUNCTION function01;
+SHOW CREATE FUNCTION function02;
+
+#delet Stored function
+DROP FUNCTION function01;
+DROP FUNCTION function02;
