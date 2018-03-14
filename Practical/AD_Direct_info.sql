7e8708c24c0d137e3378083af2e38288327dffa4
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-03-14 16:35:36 +0800

    AD & Direct info

---
+++ Practical/AD_Direct_info.sql
@@ -0,0 +1,6 @@
+select *
+from web_daily_info join user_list on web_daily_info.user_list_id = user_list.user_list_id
+where sdkid in ('') and category = 'uu'
+-- and value = 'ad' and ad_id is null
+and value = 'direct'
+and create_time between '2018-01-17' and '2018-03-12'
