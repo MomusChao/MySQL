353ca6c0f899ff97cb1ef0ced7bf7c149ab32bd3
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-03-15 16:19:23 +0800

    del data

--- Practical/Cookie_id.sql
+++
@@ -1,6 +0,0 @@
-select create_time,user_list.user_list_id , sdkids
-from web_daily_info join user_list on web_daily_info.user_list_id = user_list.user_list_id
-where sdkid in ('') and category = 'uu'
-and value = 'ad' and ad_id is null
--- and value = 'direct'
-and create_time between '2017-12-21' and '2017-12-31'
