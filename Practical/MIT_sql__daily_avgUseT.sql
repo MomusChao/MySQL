a1c13b9e0e19ec6e821eef2d9e58d06b0d84447a
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-03-15 17:53:22 +0800

    update MIT_sql__daily_avgUseT

--- Practical/MIT_sql__daily_avgUseT.sql
+++ Practical/MIT_sql__daily_avgUseT.sql
@@ -1,6 +1,6 @@
 select sum(user_count) user_count, sum(total_time) total_time, total_time/user_count avgUseTime, DATE_FORMAT(calendar.datefield ,'%Y/%m/%d')  as createTimeStr
 from (select create_time, sum(user_count) user_count, sum(total_time) total_time from web_daily_info 
-where sdkid in ('') and category = 'avgUseTime'  group by create_time)t
+where sdkid in ('BAT-') and category = 'avgUseTime'  group by create_time)t
 RIGHT JOIN calendar ON (DATE(t.create_time) = calendar.datefield) 
 where datefield >= '2018-03-01' and datefield <= '2018-03-12' 
  group by createTimeStr
