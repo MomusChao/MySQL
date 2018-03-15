c064d2cce1991a3485dad1307a4bb9555b69265c
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-03-15 17:52:33 +0800

    update MIT_one_campaign_view

--- Practical/MIT_one_campaign_view.sql
+++ Practical/MIT_one_campaign_view.sql
@@ -1,5 +1,5 @@
 select *
 from web_daily_info 
-where sdkid in ('BAT-120717-5') and category = 'view'
+where sdkid in ('BAT-') and category = 'view'
 and value = 'ad'  and ad_id in (select ad_no from ad where Campaign_Source = 'YahooNative') and device_type = 0
 and create_time between '2018-01-01' and '2018-01-16'
