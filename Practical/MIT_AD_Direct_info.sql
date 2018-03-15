f6ed9e5df27bd429e7f8e7fce22e105456acca69
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-03-15 17:47:21 +0800

    MIT: AD Direct info

--- Practical/MIT_AD_Direct_info.sql
+++ Practical/MIT_AD_Direct_info.sql
@@ -1,6 +1,6 @@
 select *
 from web_daily_info join user_list on web_daily_info.user_list_id = user_list.user_list_id
-where sdkid in ('BAT-120717-5') and category = 'uu'
+where sdkid in ('BAT-') and category = 'uu'
 -- and value = 'ad' and ad_id is null
 and value = 'direct'
 and create_time between '2018-01-17' and '2018-03-12'
